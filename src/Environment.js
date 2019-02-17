bd74c69bc4085a211e0f42bdbec797cf4583b8dd
Aaron Dalton <aaron@daltons.ca>
2019-02-17 15:45:02 -0700

    First 'real' commit after ejecting from react-create-app

---
+++ src/Environment.js
@@ -0,0 +1,31 @@
+const {
+    Environment,
+    Network,
+    RecordSource,
+    Store,
+  } = require('relay-runtime')
+
+  const store = new Store(new RecordSource())
+
+  const network = Network.create((operation, variables) => {
+    return fetch('https://api.dev.abstractplay.com/graphql', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: operation.text,
+        variables,
+      }),
+    }).then(response => {
+      return response.json()
+    })
+  })
+
+  const environment = new Environment({
+    network,
+    store,
+  })
+
+  export default environment
