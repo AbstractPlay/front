eabdebad002dab6390cf73c988c62a0de140bab5
Aaron Dalton <aaron@daltons.ca>
2019-02-23 09:16:43 -0700

    Well finally have a deployment scenario and very, very basic i18n. Will start expanding from here.

--- src/i18n.js
+++ src/i18n.js
@@ -1,21 +1,15 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-
-// the translations
-// (tip move them in a JSON file and import them)
-const resources = {
-  en: {
-    translation: {
-      "Welcome to React": "Welcome to React and react-i18next"
-    }
-  }
-};
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
+  .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    resources,
-    lng: "en",
+    fallbackLng: "en",
+    debug: true,
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
