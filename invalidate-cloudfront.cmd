e9abd13711e4b82a9911fd209907e5608691cd70
pvanwamelen <pvanwamelen@air.org>
2021-06-26 11:05:54 -0400

    Add a build-local script in package.json
    Moved invalidation of cloudfront subscription to a script so that different developers can have their own distribution ids
    Checkpoint in case npm update creates a mess...

---
+++ invalidate-cloudfront.cmd
@@ -0,0 +1,1 @@
+aws cloudfront create-invalidation --distribution-id E8GI8TIAOBUGE --paths /*
---
+++ src/config/local.js
@@ -0,0 +1,7 @@
+module.exports = {
+  COGNITO_APPID: "5tjvv8go4peaudg88oi5mrnn2v",
+  COGNITO_REDIRECT_LOGIN: "http://localhost:3000/auth",
+  COGNITO_REDIRECT_LOGOUT: "http://localhost:3000/",
+  GRAPHQL_ENDPOINT_OPEN: "https://j0jtu3nz9a.execute-api.us-east-1.amazonaws.com/dev",
+  GRAPHQL_ENDPOINT_AUTH: "https://j0jtu3nz9a.execute-api.us-east-1.amazonaws.com/dev",
+};
