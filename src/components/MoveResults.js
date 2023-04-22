892a802452ecb2d9eef5c6564fdb32455b0f3b1d
Aaron Dalton <aaron@daltons.ca>
2023-04-22 17:33:04 -0600

    Minor tweak to make player comments stand out from system-generated ones. Aesthetic fine tuning still to come.

--- src/components/MoveResults.js
+++ src/components/MoveResults.js
@@ -6,16 +6,16 @@ function MoveResults(props) {
   const players = props.players;
 
   if (results0) {
-    let results = results0.map(r => ({"time": r.time, "timestamp": new Date(r.time).getTime(), "log": r.log}))
+    let results = results0.map(r => ({"time": r.time, "timestamp": new Date(r.time).getTime(), "log": r.log, "system": true}))
     comments.forEach(c => {
-      results.push({"timestamp": c.timeStamp, "time": new Date(c.timeStamp).toLocaleString(), "log": players.find(p => p.id === c.userId).name + " commented: " + c.comment})    
+      results.push({"timestamp": c.timeStamp, "time": new Date(c.timeStamp).toLocaleString(), "log": players.find(p => p.id === c.userId).name + " commented: " + c.comment, "system": false})
     });
     results.sort((a, b) => b.timestamp - a.timestamp);
     return (
       <table className="moveResultsTable">
       <tbody>
         { results.map((r, index) =>
-          <tr key={"moveResult" + index}>
+          <tr key={"moveResult" + index} className={(r.system) ? "chatSystem" : "chatPlayer"}>
             <td>[{r.time}]</td>
             {/* Seems like JSX automatically sanitizes this, so no need to worry about XSS or use "htmlEscape". */}
             <td>{r.log}</td>
