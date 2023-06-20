bfb1cb9081f2f77cbc02ffdab313f00a8b686b82
pvanwamelen <paul.vanwamelen@cambiumassessment.com>
2023-06-20 14:18:57 -0400

    Fix for undefined game in RenderOptionsModal

--- src/components/RenderOptionsModal.js
+++ src/components/RenderOptionsModal.js
@@ -71,7 +71,7 @@ function RenderOptionsModal(props) {
   const metaName = props.game?.name;
   const gameId = props.game?.id;
   const game = props.game;
-  const cbit = game.toMove === "" || game.toMove === null ? "1" : "0";
+  const cbit = game === undefined ? undefined : game.toMove === "" || game.toMove === null ? "1" : "0";
   const settings = props.settings;
   const gameSettings = props.gameSettings;
   const show = props.show;
