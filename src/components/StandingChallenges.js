360ebe368729cde21c69cb6f971a815eb8704ec6
Aaron Dalton <aaron@daltons.ca>
2023-08-01 19:09:52 -0600

    And finally, the standing challenges table. I *think* it works.

--- src/components/StandingChallenges.js
+++ src/components/StandingChallenges.js
@@ -1,24 +1,31 @@
-import React, { useState, useEffect, useContext, Fragment } from "react";
+import React, { useState, useEffect, useContext, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import { gameinfo } from "@abstractplay/gameslib";
 import { API_ENDPOINT_OPEN, API_ENDPOINT_AUTH } from "../config";
+import { getCoreRowModel, useReactTable, flexRender, createColumnHelper, getSortedRowModel, getPaginationRowModel, getFilteredRowModel } from '@tanstack/react-table';
 import { Auth } from "aws-amplify";
 import { Helmet } from "react-helmet-async";
 import { MeContext } from "../pages/Skeleton";
 import Spinner from "./Spinner";
+import { useStorageState } from 'react-use-storage-state'
+
+const allSize = Number.MAX_SAFE_INTEGER;
 
 function StandingChallenges(props) {
   const { t } = useTranslation();
   const [loggedin, loggedinSetter] = useState(false);
-  const [challenges, challengesSetter] = useState(null);
+  const [challenges, challengesSetter] = useState([]);
   const [accepted, acceptedSetter] = useState(null);
   const [revoke, revokeSetter] = useState(null);
   const [reject, rejectSetter] = useState(null);
-  const [update, updateSetter] = useState(0);
   const [canonical, canonicalSetter] = useState("https://play.abstractplay.com/challenges/");
   const { metaGame } = useParams();
+  const [update, updateSetter] = useState(0);
   const [globalMe] = useContext(MeContext);
+  const [showState, showStateSetter] = useStorageState("challenges-show", 20);
+  const [sorting, setSorting] = useState([]);
+  const [showAccepted, showAcceptedSetter] = useState(false);
 
   useEffect(() => {
     async function fetchAuth() {
@@ -51,7 +58,11 @@ function StandingChallenges(props) {
     }
     fetchData();
     canonicalSetter(`https://play.abstractplay.com/challenges/${metaGame}/`);
-  }, [update, metaGame]);
+  }, [metaGame, update]);
+
+  useEffect(() => {
+    showAcceptedSetter((challenges !== null) && (challenges.find((c) => c.players.length > 1) !== undefined));
+  }, [challenges]);
 
   useEffect(() => {
     async function fetchData() {
@@ -90,7 +101,7 @@ function StandingChallenges(props) {
       }
     }
     if (accepted) fetchData();
-  }, [accepted, challenges, metaGame]);
+  }, [accepted, challenges, metaGame, updateSetter]);
 
   useEffect(() => {
     async function fetchData() {
@@ -126,7 +137,7 @@ function StandingChallenges(props) {
     if (revoke) {
       fetchData();
     }
-  }, [revoke, metaGame]);
+  }, [revoke, metaGame, updateSetter]);
 
   useEffect(() => {
     async function fetchData() {
@@ -183,10 +194,203 @@ function StandingChallenges(props) {
   const metaGameName = gameinfo.get(metaGame).name;
   console.log(metaGame);
   const showRespond = loggedin && challenges;
-  const showAccepters =
-    challenges && challenges.find((c) => c.players.length > 1);
+
+  const data = useMemo( () => challenges.map((rec) => {
+    return {
+        id: rec.id,
+        challenger: rec.challenger,
+        clockHard: rec.clockHard,
+        clockStart: rec.clockStart,
+        clockInc: rec.clockInc,
+        clockMax: rec.clockMax,
+        numPlayers: rec.numPlayers,
+        players: rec.players.filter(p => p.id !== rec.challenger.id),
+        rated: rec.rated,
+        seating: rec.seating,
+        variants: rec.variants,
+    }
+  }), [challenges]);
+
+  const columnHelper = createColumnHelper();
+  const columns = useMemo( () => [
+      columnHelper.accessor("challenger", {
+          header: "Challenger",
+          cell: props => props.getValue().name,
+      }),
+      columnHelper.accessor("numPlayers", {
+          header: "Players",
+      }),
+      columnHelper.accessor("players", {
+        header: "Accepted",
+        cell: props => props.getValue().map(p => p.name).join(","),
+      }),
+      columnHelper.accessor("seating", {
+          header: "Seating",
+          cell: props => props.getValue() === "random" ? t("SeatingRandom") : props.getValue() === "s1" ? t("seatingMeSecond") : t("seatingMeFirst"),
+      }),
+      columnHelper.accessor("variants", {
+          header: "Variants",
+          cell: props => props.getValue().join(", "),
+      }),
+      columnHelper.accessor("clockHard", {
+          header: "Hard clock?",
+          cell: props => props.getValue() ? t("Yes") : t("No"),
+      }),
+      columnHelper.accessor("clockStart", {
+          header: "Clock start",
+      }),
+      columnHelper.accessor("clockInc", {
+          header: "Clock increment",
+      }),
+      columnHelper.accessor("clockMax", {
+        header: "Clock max",
+      }),
+      columnHelper.accessor("rated", {
+          header: "Rated?",
+          cell: props => props.getValue() ? t("Yes") : t("No"),
+      }),
+    columnHelper.display({
+          id: "actions",
+          cell: props => (globalMe === null) ? null :
+            <>
+            {!showRespond ? null : (
+                <>
+                    {props.row.original.id === accepted ? (
+                    t("Accepted")
+                    ) : props.row.original.id === reject || props.row.original.id === revoke ? (
+                    <Spinner></Spinner>
+                    ) : props.row.original.challenger.id === globalMe?.id ? (
+                    <button
+                        className="button is-small apButton"
+                        onClick={() => handleRevoke(props.row.original.id)}
+                    >
+                        {t("Revoke")}
+                    </button>
+                    ) : props.row.original.players.find(
+                        (p) => p.id === globalMe?.id
+                    ) ? (
+                    <button
+                        className="button is-small apButton"
+                        onClick={() => handleReject(props.row.original.id)}
+                    >
+                        {t("Reject")}
+                    </button>
+                    ) : (
+                    <button
+                        className="button is-small apButton"
+                        onClick={() => handleAccept(props.row.original.id)}
+                    >
+                        {t("Accept")}
+                    </button>
+                    )}
+                </>
+            )}
+            </>
+      }),
+    ], [columnHelper, globalMe, t, accepted, revoke, reject, showRespond]);
+
+    const table = useReactTable({
+        data,
+        columns,
+        state: {
+            sorting,
+            columnVisibility: {
+                actions: globalMe !== null,
+                players: showAccepted,
+            },
+        },
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+    });
+
+    useEffect(() => {
+        table.setPageSize(showState);
+    }, [showState, table]);
+
+  const tableNavigation =
+  <>
+        <div className="level smallerText has-text-centered">
+                <div className="level-item">
+                    <button
+                        className="button is-small"
+                        onClick={() => table.setPageIndex(0)}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        <span className="icon is-small">
+                            <i className="fa fa-angle-double-left"></i>
+                        </span>
+                    </button>
+                    <button
+                        className="button is-small"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        <span className="icon is-small">
+                            <i className="fa fa-angle-left"></i>
+                        </span>
+                    </button>
+                    <button
+                        className="button is-small"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        <span className="icon is-small">
+                            <i className="fa fa-angle-right"></i>
+                        </span>
+                    </button>
+                    <button
+                        className="button is-small"
+                        onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        <span className="icon is-small">
+                            <i className="fa fa-angle-double-right"></i>
+                        </span>
+                    </button>
+                </div>
+                <div className="level-item">
+                    <p>Page <strong>{table.getState().pagination.pageIndex + 1}</strong> of <strong>{table.getPageCount()}</strong> ({table.getPrePaginationRowModel().rows.length} total challenges)</p>
+                </div>
+                {/* <div className="level-item">
+                    <div className="field">
+                        <span>|&nbsp;Go to page:</span>
+                        <input
+                            type="number"
+                            defaultValue={table.getState().pagination.pageIndex + 1}
+                            onChange={e => {
+                                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                                table.setPageIndex(page)
+                            }}
+                            className="input is-small"
+                        />
+                    </div>
+                </div> */}
+                <div className="level-item">
+                    <div className="control">
+                        <div className="select is-small">
+                            <select
+                                value={table.getState().pagination.pageSize}
+                                onChange={e => {
+                                    showStateSetter(Number(e.target.value));
+                                }}
+                                >
+                                {[10, 20, 30, 40, 50, allSize].map(pageSize => (
+                                    <option key={pageSize} value={pageSize}>
+                                    Show {pageSize === allSize ? "All" : pageSize}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+                </div>
+        </div>
+  </>
+
   return (
-    <Fragment>
+    <>
         <Helmet>
         <link rel="canonical" href={canonical} />
         </Helmet>
@@ -194,95 +398,57 @@ function StandingChallenges(props) {
       <h1 className="has-text-centered title">
         {t("StandingChallenges", { name: metaGameName })}
       </h1>
-      <div id="TableListContainer">
-        {challenges === null ? (
-          <Spinner />
-        ) : (
-          <table className="table">
-            <tbody>
-              <tr>
-                <th>{t("tblHeaderChallenger")}</th>
-                <th>{t("tblHeaderPlayers")}</th>
-                {showAccepters ? <th>{t("tblHeaderAccepted")}</th> : null}
-                <th>{t("tblHeaderSeating")}</th>
-                <th>{t("tblHeaderVariants")}</th>
-                <th>{t("tblHeaderHardClock")}</th>
-                <th>{t("tblHeaderClockStart")}</th>
-                <th>{t("tblHeaderClockIncrement")}</th>
-                <th>{t("tblHeaderClockMax")}</th>
-                <th>{t("tblHeaderRated")}</th>
-                {showRespond ? <th>{t("tblHeaderRespond")}</th> : null}
-              </tr>
-              {challenges.map((challenge, index) => (
-                <tr key={index}>
-                  <td>{challenge.challenger.name}</td>
-                  <td>{challenge.numPlayers}</td>
-                  {!showAccepters ? null : (
-                    <td>
-                      {challenge.players
-                        .filter((p) => p.id !== challenge.challenger.id)
-                        .map((p) => p.name)
-                        .join(", ")}
-                    </td>
-                  )}
-                  <td>
-                    {challenge.seating === "random"
-                      ? t("Random")
-                      : challenge.seating === "s1"
-                      ? t("ChallengerFirst")
-                      : t("ChallengerSecond")}
-                  </td>
-                  <td>
-                    {challenge.variants !== undefined &&
-                    challenge.variants.length > 0
-                      ? challenge.variants.join(", ")
-                      : ""}
-                  </td>
-                  <td>{challenge.clockHard ? t("Yes") : t("No")}</td>
-                  <td>{challenge.clockStart}</td>
-                  <td>{challenge.clockInc}</td>
-                  <td>{challenge.clockMax}</td>
-                  <td>{challenge.rated ? t("Yes") : t("No")}</td>
-                  {!showRespond ? null : (
-                    <td>
-                      {challenge.id === accepted ? (
-                        t("Accepted")
-                      ) : challenge.id === reject || challenge.id === revoke ? (
-                        <Spinner></Spinner>
-                      ) : challenge.challenger.id === globalMe?.id ? (
-                        <button
-                          className="button is-small apButton"
-                          onClick={() => handleRevoke(challenge.id)}
-                        >
-                          {t("Revoke")}
-                        </button>
-                      ) : challenge.players.find(
-                          (p) => p.id === globalMe?.id
-                        ) ? (
-                        <button
-                          className="button is-small apButton"
-                          onClick={() => handleReject(challenge.id)}
-                        >
-                          {t("Revoke")}
-                        </button>
-                      ) : (
-                        <button
-                          className="button is-small apButton"
-                          onClick={() => handleAccept(challenge.id)}
-                        >
-                          {t("Accept")}
-                        </button>
-                      )}
-                    </td>
-                  )}
+      <div className="container">
+        {tableNavigation}
+        <table className="table apTable" style={{marginLeft: "auto", marginRight: "auto"}}>
+            <thead>
+                {table.getHeaderGroups().map(headerGroup => (
+                <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => (
+                    <th key={header.id}>
+                    {header.isPlaceholder
+                        ? null
+                        : (
+                            <div
+                              {...{
+                                className: header.column.getCanSort()
+                                  ? 'sortable'
+                                  : '',
+                                onClick: header.column.getToggleSortingHandler(),
+                              }}
+                            >
+                              {flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                              )}
+                              {{
+                                asc: <>&nbsp;<i className="fa fa-angle-up"></i></>,
+                                desc: <>&nbsp;<i className="fa fa-angle-down"></i></>,
+                              }[header.column.getIsSorted()] ?? null}
+                            </div>
+                        )
+                    }
+                    </th>
+                ))}
                 </tr>
-              ))}
+                ))}
+            </thead>
+            <tbody>
+            {table.getRowModel().rows.map(row => (
+                <tr key={row.id}>
+                {row.getVisibleCells().map(cell => (
+                    <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                ))}
+                </tr>
+            ))}
             </tbody>
-          </table>
-        )}
+        </table>
+        {tableNavigation}
       </div>
     </article>
-    </Fragment>
+    </>
   );
 }
 
