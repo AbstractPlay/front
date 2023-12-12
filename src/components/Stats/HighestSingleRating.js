ddf0888f537c4e90d7694c96cd2ebdf0a09d5452
Aaron Dalton <aaron@daltons.ca>
2023-12-12 09:52:42 -0700

    Stats: Breaking ratings out by variant group and adding WLD data

--- src/components/Stats/HighestSingleRating.js
+++ src/components/Stats/HighestSingleRating.js
@@ -12,7 +12,7 @@ function HighestSingleRating(props) {
   const data = useMemo(
     () =>
       summary.ratings.highest
-        .map(({ user: userid, game, rating }) => {
+        .map(({ user: userid, game, rating, wld }) => {
           let name = "UNKNOWN";
           const user = userNames.find((u) => u.id === userid);
           if (user !== undefined) {
@@ -24,6 +24,7 @@ function HighestSingleRating(props) {
             name,
             game,
             rating,
+            wld
           };
         })
         .sort((a, b) => b.rating - a.rating),
@@ -47,6 +48,33 @@ function HighestSingleRating(props) {
       }),
       columnHelper.accessor("rating", {
         header: "Rating",
+      }),
+      columnHelper.accessor("wld", {
+        header: "Win/Loss/Draw",
+        cell: (props) => {
+            const sum = props.getValue().reduce((prev, curr) => prev + curr, 0);
+            if (sum > 0) {
+                const winrate = Math.trunc((props.getValue()[0] / sum) * 1000) / 10;
+                return `${winrate}% (${props.getValue()[0]}, ${props.getValue()[1]}, ${props.getValue()[2]})`
+            } else {
+                return `---`
+            }
+        },
+        sortingFn: (rowA, rowB, columnID) => {
+            const sumA = rowA.getValue(columnID).reduce((prev, curr) => prev + curr, 0);
+            const sumB = rowB.getValue(columnID).reduce((prev, curr) => prev + curr, 0);
+            const rateA = Math.trunc((rowA.getValue(columnID)[0] / sumA) * 1000) / 10;
+            const rateB = Math.trunc((rowB.getValue(columnID)[0] / sumB) * 1000) / 10;
+            // NaNs first
+            if (isNaN(rateA) && isNaN(rateB)) {
+                return 0;
+            } else if (isNaN(rateA)) {
+                return -1;
+            } else if (isNaN(rateB)) {
+                return 1;
+            }
+            return rateA < rateB ? -1 : rateA > rateB ? 1 : 0;
+        },
       }),
     ],
     [columnHelper, globalMe]
