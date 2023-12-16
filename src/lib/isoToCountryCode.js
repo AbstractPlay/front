67cc5e9cd5124dabefa7935f3bbc2d66d8fe8b91
Aaron Dalton <aaron@daltons.ca>
2023-12-16 13:09:25 -0700

    Fix undefined bug with country flags

--- src/lib/isoToCountryCode.js
+++ src/lib/isoToCountryCode.js
@@ -2,13 +2,16 @@ import { countryCodeList } from './countryCodeList'
 
 function isoToCountryCode(isoCode, keyToGet = "alpha2") {
   if (isoCode !== undefined) {
-    // @ts-expect-error We handle an error here properly
-    const alpha2Code = countryCodeList.find((countryObj) => (
+    const entry = countryCodeList.find((countryObj) => (
       countryObj.alpha2 === isoCode
       || countryObj.alpha3 === isoCode
       || countryObj.numeric === isoCode
-    ))[keyToGet]
-    return alpha2Code
+    ));
+    if ( (entry !== undefined) && (entry[keyToGet] !== undefined) ) {
+        return entry[keyToGet];
+    } else {
+        return undefined
+    }
   }
 
   throw new Error('This code returns undefined, see https://www.flagpack.xyz/docs/flag-index for all the available codes.')
