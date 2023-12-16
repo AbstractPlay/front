2aff4c5b2629244d9f1bedfceb296ce06633a9e0
Aaron Dalton <aaron@daltons.ca>
2023-12-16 12:30:41 -0700

    Finally updated News file

--- src/components/Player/Stars.js
+++ src/components/Player/Stars.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useMemo, useState } from "react";
+import React, { useContext, useMemo, useState } from "react";
 import { createColumnHelper } from "@tanstack/react-table";
 import { ProfileContext } from "../Player";
 import { MeContext } from "../../pages/Skeleton";
