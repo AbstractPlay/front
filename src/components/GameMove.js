284be28256630812c389965cec91b4e1463eeb20
Aaron Dalton <aaron@daltons.ca>
2024-01-03 13:59:18 -0700

    Fill insufficient palettes with white

--- src/components/GameMove.js
+++ src/components/GameMove.js
@@ -552,6 +552,9 @@ function setupColors(settings, game, globalMe) {
     const palette = globalMe.palettes.find(p => p.name === settings.color);
     if (palette !== undefined) {
         options.colours = [...palette.colours];
+        while (options.colours.length < 10) {
+            options.colours.push("#fff");
+        }
     }
   }
   game.colors = game.players.map((p, i) => {
@@ -1729,6 +1732,9 @@ function GameMove(props) {
             const palette = globalMe.palettes?.find(p => p.name === settings.color);
             if (palette !== undefined) {
                 options.colours = [...palette.colours];
+                while (options.colours.length < 10) {
+                    options.colours.push("#fff");
+                }
             }
         }
         if (gameRef.current.stackExpanding) {
