60b7adc6be04e59ec47964a361992b5b868e8772
Aaron Dalton <aaron@daltons.ca>
2024-01-19 12:06:48 -0700

    Add Reset Playground button to error screen

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1244,6 +1244,7 @@ function Playground(props) {
           errorSetter(true);
         } else {
           metaGameSetter(null);
+          errorSetter(false);
         }
       } else {
         errorMessageRef.current =
@@ -2000,7 +2001,16 @@ function Playground(props) {
       </article>
     );
   } else {
-    return <h4>{errorMessageRef.current}</h4>;
+    return (
+        <>
+            <h4>{errorMessageRef.current}</h4>);
+            <div className="control">
+            <button className="button apButton" onClick={handleResetPlayground}>
+                Reset Playground
+            </button>
+            </div>
+        </>
+    )
   }
 }
 
