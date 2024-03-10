a0dcccb74a29780a42b4013ad6f36f14e8122f09
Aaron Dalton <aaron@daltons.ca>
2024-03-10 11:29:21 -0600

    Blockade: Add board image

--- src/subscription.js
+++ src/subscription.js
@@ -66,7 +66,7 @@ export function subscribeUser(token) {
                   }
                 });
             } else {
-              console.log("Existed subscription detected.");
+              console.log("Existing subscription detected.");
               sendSubscription(token, existedSubscription);
             }
           });
