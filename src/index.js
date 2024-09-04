fa958491ffe1780087857f1ce78c9285e2df35fe
Aaron Dalton <aaron@daltons.ca>
2024-09-04 17:46:00 -0600

    Test analytics

--- src/index.js
+++ src/index.js
@@ -5,11 +5,24 @@ import * as serviceWorker from "./serviceWorker";
 import Skeleton from "./pages/Skeleton";
 import "./myBulma.css";
 import "./index.css";
+// import the GTM Module so you can access it
+import TagManager from "react-gtm-module";
 
 require("core-js/es/map");
 require("core-js/es/set");
 require("core-js/es/promise");
 require("core-js/es/object");
+
+//identify if you are on development or production
+//when you build your app process.env.NODE_ENV is set to 'production'
+const env = process.env.NODE_ENV;
+
+if (env === "production") {
+    const tagManagerArgs = {
+        gtmId: "G-TKD64QKZ99",
+    };
+    TagManager.initialize(tagManagerArgs);
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container);
