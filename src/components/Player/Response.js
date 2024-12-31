b16945324e9a25afa1ef11b6c7235e435f28ab77
Aaron Dalton <aaron@daltons.ca>
2024-12-31 12:57:41 -0700

    Change response time to be just text

--- src/components/Player/Response.js
+++ src/components/Player/Response.js
@@ -1,13 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ResponsesContext } from "../Player";
-import Plot from "react-plotly.js";
 
 function Response({ order }) {
   const [responses] = useContext(ResponsesContext);
   const [, movedSetter] = useState(0);
-  const [parsed, parsedSetter] = useState([]);
   const [avg, avgSetter] = useState(0);
   const [median, medianSetter] = useState(0);
+  const [q1, q1Setter] = useState(0);
+  const [q3, q3Setter] = useState(0);
 
   useEffect(() => {
     console.log("Update forced");
@@ -17,20 +17,24 @@ function Response({ order }) {
   useEffect(() => {
     if (responses !== null && responses.length > 0) {
         const hours = responses.map(n => n / (1000 * 60 * 60)).sort((a,b) => a - b);
-        parsedSetter(hours);
         const sum = hours.reduce((a,b) => a + b, 0);
         if (hours.length > 0) {
+            q1Setter(hours[Math.floor(hours.length * 0.25)]);
+            q3Setter(hours[Math.floor(hours.length * 0.75)]);
             avgSetter(sum / hours.length);
             medianSetter(hours[Math.floor(hours.length / 2)]);
         } else {
             avgSetter(0);
             medianSetter(0);
+            q1Setter(0);
+            q3Setter(0);
         }
     } else {
-        parsedSetter([]);
         avgSetter(0);
         medianSetter(0);
-    }
+        q1Setter(0);
+        q3Setter(0);
+}
   }, [responses]);
 
   return (
@@ -43,26 +47,10 @@ function Response({ order }) {
           <p>
             Median response time: {median.toFixed(2)} hours
           </p>
+          <p>
+            Middle half of response times: {q1.toFixed(2)} to {q3.toFixed(2)} hours
+          </p>
         </div>
-        <Plot
-          data={[
-            {
-              y: parsed.filter(n => n < 500),
-              type: "box",
-              boxpoints: false,
-              orientation: "v",
-              name: "Time to move (hours)",
-              jitter: 0.3,
-            },
-          ]}
-          config={{
-            responsive: true,
-          }}
-          layout={{
-            title: "Time to move (hours)",
-            height: 500,
-          }}
-        />
       </div>
     </>
   );
