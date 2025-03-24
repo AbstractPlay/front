1d36f621506d215cd43d0314155a5ef7cc783161
Aaron Dalton <aaron@daltons.ca>
2025-03-24 13:04:58 -0600

    Add cumulative stats to weekly game and player counts (site statistics)

--- src/components/Stats/SiteStats.js
+++ src/components/Stats/SiteStats.js
@@ -5,9 +5,32 @@ import TableSkeleton from "./TableSkeleton";
 import Plot from "react-plotly.js";
 import Flag from "../Flag";
 
+const lstSummarize = (lst) => {
+    if (lst.length === 0) {
+        return undefined;
+    }
+    const sorted = [...lst].sort((a,b) => a - b);
+    const sum = sorted.reduce((acc, curr) => acc + curr, 0);
+    const avg = sum / sorted.length;
+    let median;
+    if (sorted.length % 2 === 0) {
+        const idx1 = Math.floor(sorted.length / 2);
+        const idx2 = idx1 - 1;
+        median = (sorted[idx1] + sorted[idx2]) / 2;
+    } else {
+        median = sorted[Math.floor(sorted.length / 2)];
+    }
+    const qWidth = Math.floor(sorted.length / 4);
+    const q1 = sorted[qWidth];
+    const q3 = sorted[qWidth * 3];
+    return {avg, median, q1, q3};
+}
+
 function SiteStats({ nav }) {
   const [summary] = useContext(SummaryContext);
   const [cumulative, cumulativeSetter] = useState([]);
+  const [summaryGames, setSummaryGames] = useState(null);
+  const [summaryPlayers, setSummaryPlayers] = useState(null);
 
   useEffect(() => {
     const lst = [];
@@ -18,6 +41,8 @@ function SiteStats({ nav }) {
       lst.push(sum);
     }
     cumulativeSetter([...lst]);
+    setSummaryGames(lstSummarize(summary.histograms.all));
+    setSummaryPlayers(lstSummarize(summary.histograms.allPlayers));
   }, [summary]);
 
   const data = useMemo(
@@ -61,6 +86,25 @@ function SiteStats({ nav }) {
             The right-most bar is the most recent seven days. The left-most bar
             is the first week completed games were recorded.
           </p>
+          {summaryGames === null ? null :
+            <table>
+                <caption>Cumulative</caption>
+                <thead>
+                    <tr>
+                        <th>Average</th>
+                        <th>Median</th>
+                        <th>Middle half</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{summaryGames.avg.toLocaleString(undefined, {maximumFractionDigits: 2})}</td>
+                        <td>{summaryGames.median.toLocaleString(undefined, {maximumFractionDigits: 2})}</td>
+                        <td>{summaryGames.q1.toLocaleString(undefined, {maximumFractionDigits: 2})}–{summaryGames.q3.toLocaleString(undefined, {maximumFractionDigits: 2})}</td>
+                    </tr>
+                </tbody>
+            </table>
+        }
         </div>
         <Plot
           data={[
@@ -84,40 +128,29 @@ function SiteStats({ nav }) {
       <div>
         <div className="content">
           <p>
-            Total cumulative timeout rate is{" "}
-            {summary.timeoutRate.toLocaleString(undefined, {
-              style: "percent",
-              minimumFractionDigits: 2,
-            })}
-            .
-          </p>
-        </div>
-        <Plot
-          data={[
-            {
-              y: [...summary.histograms.timeouts].reverse(),
-              type: "bar",
-            },
-          ]}
-          config={{
-            responsive: true,
-          }}
-          layout={{
-            title: "Timeout rate per week",
-            xaxis: { title: "Week #" },
-            yaxis: { title: "Timeout rate", fixedrange: true, range: [0, 1] },
-            height: 500,
-          }}
-        />
-        <hr />
-      </div>
-      <div>
-        <div className="content">
-          <p>
             Games are counted in seven-day chunks from the date the script ran.
             The right-most bar is the most recent seven days. The left-most bar
             is the first week completed games were recorded.
           </p>
+          {summaryPlayers === null ? null :
+            <table>
+                <caption>Cumulative</caption>
+                <thead>
+                    <tr>
+                        <th>Average</th>
+                        <th>Median</th>
+                        <th>Middle half</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{summaryPlayers.avg.toLocaleString(undefined, {maximumFractionDigits: 2})}</td>
+                        <td>{summaryPlayers.median.toLocaleString(undefined, {maximumFractionDigits: 2})}</td>
+                        <td>{summaryPlayers.q1.toLocaleString(undefined, {maximumFractionDigits: 2})}–{summaryPlayers.q3.toLocaleString(undefined, {maximumFractionDigits: 2})}</td>
+                    </tr>
+                </tbody>
+            </table>
+        }
         </div>
         <Plot
           data={[
@@ -179,6 +212,36 @@ function SiteStats({ nav }) {
           data={data}
           columns={columns}
           sort={[{ id: "n", desc: true }]}
+        />
+        <hr />
+      </div>
+      <div>
+        <div className="content">
+          <p>
+            Total cumulative timeout rate is{" "}
+            {summary.timeoutRate.toLocaleString(undefined, {
+              style: "percent",
+              minimumFractionDigits: 2,
+            })}
+            .
+          </p>
+        </div>
+        <Plot
+          data={[
+            {
+              y: [...summary.histograms.timeouts].reverse(),
+              type: "bar",
+            },
+          ]}
+          config={{
+            responsive: true,
+          }}
+          layout={{
+            title: "Timeout rate per week",
+            xaxis: { title: "Week #" },
+            yaxis: { title: "Timeout rate", fixedrange: true, range: [0, 1] },
+            height: 500,
+          }}
         />
         <hr />
       </div>
