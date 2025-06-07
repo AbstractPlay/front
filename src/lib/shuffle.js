7f9ae7889e8b36da538f75fad38eb1ecbccafeeb
Aaron Dalton <aaron@daltons.ca>
2025-06-07 15:36:09 -0600

    Tweak About wording

--- src/lib/shuffle.js
+++ src/lib/shuffle.js
@@ -1,18 +1,17 @@
 export const shuffle = (lst) => {
-    const working = [...lst];
-    let remaining = working.length;
+  const working = [...lst];
+  let remaining = working.length;
 
-    // While there remain elements to shuffle…
-    while (remaining) {
+  // While there remain elements to shuffle…
+  while (remaining) {
+    // Pick a remaining element…
+    const randomIdx = Math.floor(Math.random() * remaining--);
 
-        // Pick a remaining element…
-        const randomIdx = Math.floor(Math.random() * remaining--);
+    // And swap it with the current element.
+    const t = working[remaining];
+    working[remaining] = working[randomIdx];
+    working[randomIdx] = t;
+  }
 
-        // And swap it with the current element.
-        const t = working[remaining];
-        working[remaining] = working[randomIdx];
-        working[randomIdx] = t;
-    }
-
-    return working;
-}
+  return working;
+};
