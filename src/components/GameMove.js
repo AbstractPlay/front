d54d67e5838508e822507b09522b381f3dc654c5
M. C. DeMarco <mcd@mcdemarco.net>
2026-02-19 16:58:10 -0500

    Unblanch previous fix

--- src/components/GameMove.js
+++ src/components/GameMove.js
@@ -1403,8 +1403,7 @@ function GameMove(props) {
         }
         // extend all palettes to 12 colours
         if (
-          optioncolours !== undefined &&
-          Array.isArray(optioncolours) &&
+          optioncolours.length > 0 &&
           optioncolours.length < 12
         ) {
           while (optioncolours.length < 12) {
