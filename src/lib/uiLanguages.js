2bd0a72411fb2a96abfd5bd2e3a10ed108335e95
Aaron Dalton <aaron@daltons.ca>
2026-08-26 18:12:20 -0600

    Trying again

--- src/lib/uiLanguages.js
+++
@@ -1,8 +0,0 @@
-export const SUPPORTED_LANGUAGES = [
-  { code: "en", label: "English" },
-  { code: "eo", label: "Esperanto" },
-  { code: "fr", label: "Français" },
-  { code: "de", label: "Deutsch" },
-  { code: "it", label: "Italiano" },
-  { code: "es", label: "Español" },
-];
